25b253d48c6a48476c48129c3f20127602182746
Stockmann <stockmanngabriel1@proton.me>
2026-05-01 12:52:19 -0300

    Resultado final

--- petri_net.py
+++ petri_net.py
@@ -42,14 +42,14 @@ def initial_mark(places):
             except ValueError:
                 print('\n[!] ERRO: Digite um valor inteiro!\n')
 
-    return np.array(x)
+    return tuple(x)
 
 
 def pode_disparar(A_out, x, transicao):
-    for p in range(x):
-        if x[p] >= A_out[p][transicao]:
-            return True
-    return False
+    for p in range(len(x)):
+        if x[p] < A_out[p][transicao]:
+            return False
+    return True
 
 
 def disparo(A_out, A_in, x, t):
@@ -66,6 +66,54 @@ def main():
     A_out = define_matriz(places, transicoes)
     A_in = define_matriz(places, transicoes)
     x_ = initial_mark(places)
+
+    queue = [x_]
+    visited = {x_}
+    edges = []
+    ancestors = {x_: None}
+
+    while queue:
+        current_mrk = queue.pop(0)
+
+        for t in range(transicoes):
+            if pode_disparar(A_out, current_mrk, t):
+                next_x = disparo(A_out, A_in, current_mrk, t)
+
+                prox_mark = list(next_x)
+                father = current_mrk
+
+                while father is not None:
+                    if all(prox_mark[i] >= father[i] for i in range(places)) and any(prox_mark[i] > father[i] for i in range(places)):
+                        for j in range(places):
+                            if prox_mark[j] > father[j]:
+                                prox_mark[j] = float('inf')
+                    father = ancestors.get(father)
+
+                next_x = tuple(prox_mark)
+                edges.append((current_mrk, f'{t + 1}', next_x))
+
+                if next_x not in visited:
+                    visited.add(next_x)
+                    ancestors[next_x] = current_mrk
+                    queue.append(next_x)
+
+    print("\n--- Árvore de Cobertura Final ---")
+    if not edges:
+        print("Nenhuma transição habilitada")
+
+    for x, trans, x_prime in edges:
+        # Função para limpar a marcação: remove np.float, converte inf para ω e remove .0
+        def limpar_marcacao(m):
+            elementos = []
+            for val in m:
+                if val == float('inf'):
+                    elementos.append('ω')
+                else:
+                    # int(val) remove o .0 e o rótulo do numpy
+                    elementos.append(str(int(val)))
+            return "(" + ", ".join(elementos) + ")"
+
+        print(f"{limpar_marcacao(x)} --({trans})--> {limpar_marcacao(x_prime)}")
 
 
 main()
